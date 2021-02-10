9315484ba8139c8657307c9a9e30993e4197061e
unknown <sharonkovich@gmail.com>
2021-02-10 19:22:19 +0200

    Added 'test_registration_with_existing_email' test

--- Tests/Registration_API_Tests.py
+++ Tests/Registration_API_Tests.py
@@ -10,3 +10,8 @@ class APItests(unittest.TestCase):
     def test_successful_registration(self):
         result = self.registration_endpoint.send_post_request(data=self.valid_data)
         self.assertEqual(201, result.status_code, msg=result.content)
+
+    def test_registration_with_existing_email(self):
+        self.valid_data["email"] = "test@gmail.com"
+        result = self.registration_endpoint.send_post_request(data=self.valid_data)
+        self.assertNotEqual(201, result.status_code, result.content)
