5ec3c8b790148aa8a1ce7706399a6ae75d57bff3
unknown <sharonkovich@gmail.com>
2021-02-10 15:31:10 +0200

    Added test_registration_with_existing_email test

--- Tests/Registration_UI_Tests.py
+++ Tests/Registration_UI_Tests.py
@@ -21,3 +21,12 @@ class UItests(unittest.TestCase):
         WebDriverWait(self.register_page_handler.browser, 100).until(EC.url_contains('login'))
         url = self.register_page_handler.get_current_url()
         self.assertEqual(url, 'https://sharonkrochkovich.herokuapp.com/#/login')
+
+    def test_registration_with_existing_email(self):
+        self.register_page_handler.set_email(email='test@gmail.com')
+        self.register_page_handler.set_password()
+        self.register_page_handler.set_security_question()
+        self.register_page_handler.set_security_answer()
+        self.register_page_handler.click_register_button()
+        error_message = self.register_page_handler.get_api_validation_error('Email must be unique')
+        self.assertEqual(error_message, 'Email must be unique')
